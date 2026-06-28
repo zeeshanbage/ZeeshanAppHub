6af5bf37608800a46d75161c4040a79077a6e4cd
zeeshanbage <zeeshanbge@gmail.com>
2026-06-28 21:18:06 +0530

    Fix typescript build compilation by removing unused actions and adding app-info-parser declaration

--- webapp/src/types/app-info-parser.d.ts
+++ webapp/src/types/app-info-parser.d.ts
@@ -1,10 +1,20 @@
 declare module 'app-info-parser' {
     class AppInfoParser {
-        constructor(filePath: string);
+        constructor(filePath: string | File);
         parse(): Promise<{
             icon: string;
+            package: string;
+            versionName: string;
+            application: {
+                label?: string | string[];
+            };
             [key: string]: any;
         }>;
     }
+    export default AppInfoParser;
+}
+
+declare module 'app-info-parser/dist/app-info-parser.js' {
+    import AppInfoParser from 'app-info-parser';
     export default AppInfoParser;
 }
